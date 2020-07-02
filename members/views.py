f74186d135c94702026081fb523a290215c15b47
AmoahDevLabs <amoahdevlabs@gmail.com>
2020-07-02 14:19:26 +0000

    Upadtes on db, views. touch of edit_profile

--- members/views.py
+++ members/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm
 
@@ -9,3 +9,10 @@ class UserRegisterView(CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(CreateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
