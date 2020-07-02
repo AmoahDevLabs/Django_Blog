2feaf1aca8f14a6fd66a1e318503f7a1bf904d1e
AmoahDevLabs <amoahdevlabs@gmail.com>
2020-07-02 10:26:10 +0000

    Upadte on home.html

--- blog/forms.py
+++ blog/forms.py
@@ -17,7 +17,7 @@ for choice in choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'category', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
@@ -26,6 +26,7 @@ class PostForm(forms.ModelForm):
             # 'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control'})
         }
 
 
