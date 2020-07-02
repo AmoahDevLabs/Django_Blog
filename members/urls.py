bef5cc603d0246728bd02cc8ab0e882587c9a602
AmoahDevLabs <amoahdevlabs@gmail.com>
2020-07-02 16:10:17 +0000

    Some #commented classse not in use!

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import UserRegisterView, UserEditView, PasswordsChangeView
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
--- members/views.py
+++ members/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.views import PasswordChangeView
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+# from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
 
