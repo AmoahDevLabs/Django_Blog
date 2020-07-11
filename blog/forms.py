78d1a9cf968e2de4ffbe92e1ef7972b0dbec1e78
AmoahDevLabs <amoahdevlabs@gmail.com>
2020-07-11 00:14:48 +0000

    Multiple commits

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post, Category
+from .models import Post, Category, Profile
 
 '''choices = [('coding', 'Coding'),
            ('entertainment', 'Entertainment'),
--- members/forms.py
+++ members/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django import forms
+from blog.models import Profile
 
 
 class SignUpForm(UserCreationForm):
@@ -40,3 +41,18 @@ class PasswordChangingForm(PasswordChangeForm):
     class Meta:
         model = User
         fields = ('old_password', 'new_password1', 'new_password2')
+
+
+class UpdateProfileForm(UserChangeForm):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url')
+
+        widgets = {
+            'bio': forms.Textarea(attrs={'class': 'form-control'}),
+            'website_url': forms.TextInput(attrs={'class': 'form-control'}),
+            'facebook_url': forms.TextInput(attrs={'class': 'form-control'}),
+            'twitter_url': forms.TextInput(attrs={'class': 'form-control'}),
+            'instagram_url': forms.TextInput(attrs={'class': 'form-control'}),
+
+        }
