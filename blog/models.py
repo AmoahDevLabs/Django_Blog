8bd5812504c80cafc544ea04e1ba0e0b56c8b23c
AmoahDevLabs <amoahdevlabs@gmail.com>
2020-07-17 00:43:53 +0000

    Add ups...

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime, date
 from django.urls import reverse
 from ckeditor.fields import RichTextField
 
@@ -52,3 +51,13 @@ class Post(models.Model):
     def get_absolute_url(self):
         # return reverse('article_detail', args=(str(self.id)))
         return reverse('home')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
